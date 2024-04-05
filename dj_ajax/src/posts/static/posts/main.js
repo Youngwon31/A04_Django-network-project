dc38748f7bc4030e8f1762d6ac70f9ea1828f4c4
Youngwon31 <115308850+Youngwon31@users.noreply.github.com>
2024-04-05 17:38:55 -0400

    Implement post listing and dynamic content loading with AJAX in Django and add loading spinner during AJAX calls

---
+++ dj_ajax/src/posts/static/posts/main.js
@@ -0,0 +1,43 @@
+console.log('hello world')
+
+const helloWorldBox = document.getElementById('hello-world')
+const postsBox = document.getElementById('posts-box')
+const spinnerBox = document.getElementById('spinner-box')
+
+
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response){
+        console.log('success', response.text)
+        helloWorldBox.textContent = response.text
+    },
+    error: function(error){
+        console,log('error', error)
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response){
+        console.log(response)
+        const data = response.data
+        setTimeout(()=>{
+            spinnerBox.classList.add('not-visible')
+            console.log(data)
+            data.forEach(el => {
+            postsBox.innerHTML += `
+                 ${el.title} - <b>${el.body}</b><br>
+             `
+         });
+        }, 1000)
+        // const data = JSON.parse(response.data)
+        // console.log(data)
+    },
+    error: function(error){
+        console,log('error', error)
+    }
+
+})
