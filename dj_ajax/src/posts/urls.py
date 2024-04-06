3948ca5bbb696f141bcd19ceeaf78e09deee9dad
Youngwon31 <115308850+Youngwon31@users.noreply.github.com>
2024-04-06 00:52:16 -0400

    Implement post detail page functionality with AJAX and dynamically load content without page refresh

--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -3,8 +3,9 @@ from .views import (
     post_list_and_create,
     load_post_data_view,
     like_unlike_post,
+    post_detail,
 
-    hello_world_view
+    post_detail_data_view,
 )
 
 app_name = 'posts'
@@ -12,8 +13,12 @@ app_name = 'posts'
 urlpatterns = [
     path('', post_list_and_create, name='main-board'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
-    path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
+    path('<pk>/', post_detail, name='post-detail'),
 
-    path('hello-world/', hello_world_view, name='hello-world'),
+
+    path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
+    path('<pk>/data/', post_detail_data_view, name='post_detail_data_view'),
+
+
 
 ]
